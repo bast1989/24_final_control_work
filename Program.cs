31dc48640a70336af7295e5c0816f1c2d98019e1
Aleksandr Markov <bast.ne@mail.ru>
2023-04-06 21:14:54 +0700

    function add PrintTwoArrays + minor changes ThreeAndUnder

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ string[] ThreeAndUnder(string[] arr)
 
     if (iter <= 0) //если элементов с количеством символов 3 и меньше нет, создаём пустой массив myArr
     {
-        return myArr = new string[] {" "}; 
+        return myArr = new string[] {}; 
     }
     else //если элементы с количеством символов 3 и меньше есть, заполняем массив myArr ими
     {
@@ -28,15 +28,63 @@ string[] ThreeAndUnder(string[] arr)
             if (arr[i].Length <= 3)
             {
                 myArr[iter] = arr[i];
-                i++;
+                iter++;
             }            
         }
         return myArr;
     }
 }
 
-ThreeAndUnder(arr_1);
-Console.WriteLine();
-ThreeAndUnder(arr_2);
-Console.WriteLine();
-ThreeAndUnder(arr_3);
+void PrintTwoArrays(string[] originalArr, string[] editedArr)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < originalArr.Length; i++)
+    {
+        Console.Write($"\"{originalArr[i]}\"");
+        if (i < originalArr.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+
+    Console.Write("]");
+
+    Console.Write(" -> ");
+
+    Console.Write("[");
+
+    for (int j = 0; j < editedArr.Length; j++)
+    {
+        Console.Write($"\"{editedArr[j]}\"");
+        if (j < editedArr.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+
+    Console.Write("]");
+}
+
+
+
+
+
+
+
+string[] newArr_1 = ThreeAndUnder(arr_1);
+PrintTwoArrays(arr_1, newArr_1);
+
+Console.WriteLine("\n");
+
+string[] newArr_2 = ThreeAndUnder(arr_2);
+PrintTwoArrays(arr_2, newArr_2);
+
+Console.WriteLine("\n");
+
+string[] newArr_3 = ThreeAndUnder(arr_3);
+PrintTwoArrays(arr_3, newArr_3);
+
+
+
+
